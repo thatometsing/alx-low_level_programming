be162cfb54a86e2c8da28670b8bda1716200072f
Thato Metsing <mtstha052@myuct.ac.za>
2022-09-09 07:12:29 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,8 +8,6 @@
 
 int main(void)
 {
-	char str[] = "with proper grammar, but the outcome is a piece of art,";
-
-	printf("%c", str);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
