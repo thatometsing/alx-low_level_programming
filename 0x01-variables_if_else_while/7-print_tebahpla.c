bc8f8b32fab0df6818ea677d8c96ddbcc1d2a86c
Thato Metsing <mtstha052@myuct.ac.za>
2022-09-10 06:00:03 -0700

    opposite alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = 122; i <= 97; i--)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
