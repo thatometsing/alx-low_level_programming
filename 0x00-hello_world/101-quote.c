a9d93d6e0dd44aaf16f85e794c6fa354064a7fa1
thato <mtstha052@myuct.ac.za>
2022-12-18 03:40:15 -0800

    committing the final advance problem 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints exactly and that piece of art is useful" - Dora Korpar,
+ * 2015-10-19, followed by a new line, to the standard erro Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write(s, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
