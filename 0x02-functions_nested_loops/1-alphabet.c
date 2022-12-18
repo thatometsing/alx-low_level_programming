98f73db6c3b7e08535031c0cd789c896f2f6e893
thatometsing <mtstha052@myuct.ac.za>
2022-12-18 09:40:45 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
-njnm
+#include "main.h"
+
+/**
+ * print_alphabet - prints all alphabet then a new line
+ */
+
+void print_alphabet(void)
+{
+	char az;
+
+	for (az = 'a', az <= 'z', az++)
+	{
+		_putchar(az);
+	}
+	putchar('n');
+	return (0);
+}
