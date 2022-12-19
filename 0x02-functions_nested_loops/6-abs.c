01649b667b9ab3318bef842ba801eca54f7cd642
thatometsing <mtstha052@myuct.ac.za>
2022-12-19 08:14:26 -0800

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,18 +6,10 @@
  * Return: the absolute value of the integer.
  */
 
-int _abs(int)
+int _abs(int n)
 {
-	if (n > 0)
-	{
+	if (n >= 0)
 		return (n);
-	}
-	else if (n == 0)
-	{
-		return (n);
-	}
 	else
-	{
 		return (-n);
-	}
 }
