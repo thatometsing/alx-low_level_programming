2b71ac7ee519454544409fab7e58e07ea80da117
thatometsing <mtstha052@myuct.ac.za>
2022-12-20 04:22:56 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update a parameter value using a pointer
+ * @n: int we will update
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Lets learm pinters :)
